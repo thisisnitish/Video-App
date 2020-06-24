e4582721e078552ee9ca5623dd8406248b9db054
Nitish Kumar <nitishkumargenius@gmail.com>
2020-06-24 18:03:05 +0530

    Final update

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import youtube from '../apis/youtube';
 import VideoList from './VideoList';
 import VideoDetail from './VideoDetail';
 
-const KEY = 'AIzaSyCS6PI0d-fhDaIKpT8rAI2sxmZjlCoZWWU';
+const KEY = 'YOUR_YOUTUBE_API_KEY';
 
 
 class App extends React.Component {
